e861091aee4d16d0932bd722a2c336d3ab5b1c83
gorgeusgeorge <ilikepython.really@gmail.com>
2022-08-11 19:14:58 +0300

    parsing

--- mysite/celery.py
+++ mysite/celery.py
@@ -3,6 +3,8 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
+from celery.schedules import crontab
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 app = Celery('mysite')
@@ -17,6 +19,17 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
+
+# Celery beat configuration
+app.conf.beat_schedule = {
+    'parse_5_quotes_every_odd_hour': {
+        'task': 'test_celery.tasks.parse_5_quotes',
+        'schedule': crontab(minute=0, hour="1-23/2"),
+    },
+}
+
+
+app.conf.timezone = 'Europe/Kiev'
 
 @app.task(bind=True)
 def debug_task(self):
--- test_celery/tasks.py
+++ test_celery/tasks.py
@@ -1,7 +1,12 @@
-# Create your tasks here
+import requests
+
+from bs4 import BeautifulSoup
+
 from celery import shared_task
 
 from django.core.mail import send_mail
+
+from test_celery.models import Author, Quote
 
 
 @shared_task
@@ -13,3 +18,50 @@ def send_email_task(subject, message_text, recipient_email):
         [recipient_email],
         fail_silently=False,
     )
+
+
+@shared_task
+def parse_5_quotes():
+    base_url = 'https://quotes.toscrape.com/'
+    r = requests.get(base_url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    next_button_url = soup.find('li', {'class': 'next'}).a.get('href')
+    # loop for pagination
+    while True:
+        # counter to stop the loop after 5 quotes saved
+        counter = 0
+        while counter != 5:
+            # take all quotes on the page
+            parsed_quotes = soup.find_all('div', {'class': 'quote'})
+            # iteration through each quote on the page
+            for quote in parsed_quotes:
+                quote_text = quote.span.text
+                author_name = quote.small.text
+                quote_id = Quote.objects.get(quote_text=quote_text)
+                # check if the quote already exist in the database
+                if not quote_id:
+                    # save author using get_or_create
+                    r2 = requests.get(base_url+quote.a.get('href'))
+                    soup2 = BeautifulSoup(r2.content, 'html.parser')
+                    author_description = soup2.find('div', {'class': 'author-description'}).text
+                    obj, created = Author.objects.get_or_create(
+                        author_name=author_name,
+                        defaults={'author_description': author_description},
+                    )
+                    # save the quote
+                    author_id = Author.objects.get(author_name=author_name)
+                    q = Quote.objects.create(quote_text=quote_text, authors_id=author_id)
+                    counter += 1
+        # go to next page or break if this is the last page
+        r = requests.get(base_url + next_button_url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        next_button_url = soup.find('li', {'class': 'next'}).a.get('href')
+        if not next_button_url:  # so this is the last page
+            break
+        send_mail(
+            'parsing task completed',
+            'all quotes have been parsed',
+            'sender@abc.com',
+            'recepient@abc.com',
+            fail_silently=False,
+        )
