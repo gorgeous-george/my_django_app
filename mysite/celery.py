67728a2266a56fc0dea0f2a4e7275c64fab1597f
gorgeusgeorge <ilikepython.really@gmail.com>
2022-08-12 11:31:03 +0300

    schedule updated

--- mysite/celery.py
+++ mysite/celery.py
@@ -24,8 +24,7 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'parse_5_quotes_every_odd_hour': {
         'task': 'test_celery.tasks.parse_5_quotes',
-        'schedule': crontab(minute=1),
-        # 'schedule': crontab(minute=0, hour="1-23/2"),
+        'schedule': crontab(minute=0, hour="1-23/2"),
     },
 }
 
