cf5813a910899914b675d9341012a8bfb014674d
gorgeusgeorge <ilikepython.really@gmail.com>
2022-08-12 12:16:48 +0300

    parsing updated

--- test_celery/models.py
+++ test_celery/models.py
@@ -5,7 +5,12 @@ class Author(models.Model):
     author_name = models.CharField(max_length=30)
     author_description = models.TextField()
 
+    def __str__(self):
+        return self.author_name
 
 class Quote(models.Model):
     quote_text = models.CharField(max_length=250)
     authors = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.quote_text
