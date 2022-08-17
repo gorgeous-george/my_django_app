5c02274ea356dfed3d0d7becdb5b1772a1d839ab
gorgeusgeorge <ilikepython.really@gmail.com>
2022-08-17 10:51:59 +0300

    practiced with cache

--- test_celery/urls.py
+++ test_celery/urls.py
@@ -1,11 +1,11 @@
 from django.views.decorators.cache import cache_page
 from django.urls import path
 
-from test_celery.views import AuthorView, send_email, send_email_failure, send_email_success
+from test_celery.views import QuoteView, send_email, send_email_failure, send_email_success
 
 urlpatterns = [
     path('', send_email, name="send-email"),
     path('success/', send_email_success, name="send-email-success"),
     path('failure/', send_email_failure, name="send-email-failure"),
-    path('cached_quotes/', cache_page(10)(AuthorView.as_view()), name="cached-quotes"),
+    path('cached_quotes/', cache_page(10)(QuoteView.as_view()), name="cached-quotes"),
 ]
